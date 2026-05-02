60519b15f1995f7e497944561cfa933c9152bff4
waddah82 <wmahnam@gmail.com>
2026-05-02 21:02:21 +0300

    Add reset_mode_of_payments to SalesInvoice

--- wmn/__init__.py
+++ wmn/__init__.py
@@ -1,1 +1,17 @@
+import frappe
+from erpnext.accounts.doctype.sales_invoice.sales_invoice import SalesInvoice
+from erpnext.accounts.doctype.sales_invoice.sales_invoice import update_multi_mode_option
+
 __version__ = "0.0.1"
+
+
+@frappe.whitelist()
+def reset_mode_of_payments(self):
+    if self.pos_profile:
+        pos_profile = frappe.get_cached_doc("POS Profile", self.pos_profile)
+        update_multi_mode_option(self, pos_profile)
+        self.paid_amount = 0
+
+
+if not hasattr(SalesInvoice, "reset_mode_of_payments"):
+    SalesInvoice.reset_mode_of_payments = reset_mode_of_payments
