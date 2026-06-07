ff3714970a463b027ba4bdd872d3ea77881b0f3e
waddah82 <wmahnam@gmail.com>
2026-06-08 01:01:35 +0300

    Add global_new_override.js to hooks.py

--- wmn/hooks.py
+++ wmn/hooks.py
@@ -17,6 +17,7 @@ app_include_js = [
     "/assets/wmn/js/pos_barcode_override.js",
     "assets/wmn/js/workspace_header_unified.js",
     "/assets/wmn/js/silent_print.js",
+    "/assets/wmn/js/global_new_override.js",
     #"assets/wmn/js/html2canvas.js",
     #"assets/wmn/js/pdfmake.min.js",
     #"assets/wmn/js/vfs_fonts.js",
